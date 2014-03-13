122e884abeca49e3d1d1519a4784677ca76226ea
meuhia <willykonguem@yahoo.fr>
2014-03-13 17:16:29 +0100

    correct url

--- Simplan/event/models.py
+++ Simplan/event/models.py
@@ -92,7 +92,7 @@ class Event(models.Model):
     
     def get_absolute_url(self):
         return settings.SITE_URL+reverse('Simplan.event.views.view_event', args=[
-            self.slug_public
+            self.slug_public[0:8], self.slug_public[8:12], self.slug_public[12:16], self.slug_public[16:20], self.slug_public[20:32]
         ])
     
     def get_absolute_admin_url(self):
